a0bf275bbaa69868c34907f335b64d436759a6a4
Lars Ippen <larsippen@hotmail.de>
2024-03-04 00:56:22 +0100

    feat: Use functionality from https://www.studydrive.lol to generate links. Credits: https://github.com/sponsors/Curve

--- studydrive_downloader.py
+++ studydrive_downloader.py
@@ -1,7 +1,7 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, request, render_template, url_for
-from processing import get_file_preview_url, counter_increase, get_counter, ensure_https_www_prefix, is_valid_url
+from processing import counter_increase, get_counter, ensure_https_www_prefix, is_valid_url, get_lol_url
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static')
 
@@ -19,17 +19,9 @@ def adder_page():
 
         url = ensure_https_www_prefix(url)
         if url is not None and is_valid_url(url):
-
-            result = get_file_preview_url(url)
-            if result == -1 or result == "https://www.studydrive.net/guest.pdf":
-                error_msg = "Error: The provided Link does not contain a supported file format"
-            elif result is None:
-                error_msg = "Unknown Error"
-            else:
-                counter_increase()
-
-                return render_template("Result-Page-1.html", result=result, counter=get_counter())
-
+            result = get_lol_url(url)
+            counter_increase()
+            return render_template("Result-Page.html", result=result, counter=get_counter())
         else:
             error_msg = "Error: The provided Link is not a Studydrive File Link (e.g. \"studydrive.net/en/doc/doc_name/123456\")"
 
